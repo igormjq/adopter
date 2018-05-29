90e3600a0c1d67de2a8aaeddbf03c907a1d98f37
Igor <igormjq@gmail.com>
2018-05-29 00:37:38 -0300

    auth middleware added

---
+++ src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import User from '../models/user'
+
+const auth = (req, res, next) => {
+  let token = req.header('x-auth');
+
+  User.findByToken(token)
+    .then(user => {
+      
+      if(!user)
+        return Promise.reject()
+      
+      req.user = user;
+      req.token = token;
+
+      next()
+    })
+    .catch(err => res.status(401).send({ status: 401, message: 'Token inválido' }));
+
+
+};
+
+export default auth;
