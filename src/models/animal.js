6bdbce29beb9dd4c14def33da230f14b214a851e
Igor <igormjq@gmail.com>
2018-05-30 16:00:47 -0300

    animal model rebuilt

--- src/models/animal.js
+++ src/models/animal.js
@@ -1,10 +1,47 @@
 import mongoose from 'mongoose';
 
+const { ObjectId } = mongoose.Schema.Types;
+
 const AnimalSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     trim: true
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['dog', 'cat']
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female']
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ['xs', 's', 'm', 'l', 'xl']
+  },
+  castrated: {
+    type: Boolean
+  },
+  vacinated: {
+    type: Boolean
+  },
+  isAvailabe: {
+    type: Boolean
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+  },
+  _creator: {
+    type: ObjectId,
   }
 });
 
