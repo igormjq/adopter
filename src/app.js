153b26f74acf50ce9de1e12722f40848c8832f40
Igor <igormjq@gmail.com>
2018-06-13 01:26:57 -0300

    allow cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import config from '../config/config';
 import mongoose from '../config/database';
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
