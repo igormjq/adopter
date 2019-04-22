df4b196a7c1934d3c05bc0ab13bc873250fb9d55
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-04-22 13:09:37 -0300

    :new: Update user mutation added to API

--- src/resolvers/user/UserMutations.js
+++ src/resolvers/user/UserMutations.js
@@ -2,6 +2,8 @@ import bcrypt from 'bcryptjs';
 
 export default {
   async createUser (parent, { data }, { prisma }, info) {
+    if(!password) throw new Error('Senha é obrigatório');
+
     const password = await bcrypt.hash(data.password, 10);
     const user = await prisma.mutation.createUser({
       data: {
@@ -11,5 +13,11 @@ export default {
     });
 
     return user;
+  },
+  async updateUser(parent, { id, data }, { prisma }, info) {
+    return prisma.mutation.updateUser({
+      where: { id },
+      data
+    }, info);
   }
 }
