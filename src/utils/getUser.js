9819c77f1c3118169f1d670e7e73fe152abf9b15
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-04-30 18:31:41 -0300

    :recycle: Wrap in try/catch the logics for extracting user from request

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -1,9 +1,15 @@
 import jwt from 'jsonwebtoken';
 
 export default header => {
-  if(!header) throw new Error('Não autenticado');
-  const token = header.replace('Bearer ', '');
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  let decoded;
 
-  return decoded.id;
+  if(!header) throw new Error('Não autorizado');
+
+  try {
+    const token = header.replace('Bearer ', '');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.id;
+  } catch(e) {
+    throw new Error('Token inválido');
+  }
 }
