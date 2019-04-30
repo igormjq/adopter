ff7c65304c2a11a52b7e9b229b1f1e8c9ed8dd91
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-04-30 18:37:57 -0300

    :sparkles: Reset master branch

--- config/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const uri = process.env.MONGODB_URI;
-
-mongoose.Promise = global.Promise;
-mongoose.connect(uri);
-
-export default { mongoose };
--- www/index.js
+++
@@ -1,7 +0,0 @@
-import app from '../src/app';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Adopter API running on port ${PORT}`);
-});
