87fd585c8e9f295e24892511dd8e82459d16e1f3
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-02 10:35:11 -0300

    :art: :truck: Separate seeder files

--- db/seeder/index.js
+++ db/seeder/index.js
@@ -1,6 +1,6 @@
-import prisma from '../../src/prisma';
-import bcrypt from 'bcryptjs';
 import chalk from 'chalk';
+import createRoles from './create-roles';
+import createUsers from './create-users';
 
 const log = console.log;
 
@@ -8,82 +8,11 @@ const seedDatabase = async () => {
   log(chalk.green('Seeding database'));
 
   try {
-    log(chalk.yellow('Creating Users...'));
-
-    await prisma.mutation.createUser({
-      data: {
-        name: "Igor Marini Jaekel Quevedo",
-        email: "igormjq@gmail.com",
-        password: (await bcrypt.hash('adopter', 10)),
-        phone: "+5553981189503",
-        role: {
-          create: {
-            name: "ADMIN",
-            displayName: "Administrador"
-          }
-        },
-        address: {
-          create: {
-            street: "Rua Major Cícero de Góes Monteiro",
-            number: "926",
-            district: "Centro",
-            city: "Pelotas",
-            uf: "RS"
-          }
-        }
-      }
-    });
-
-    await prisma.mutation.createUser({
-      data: {
-        name: "Miguel Novack Boanova",
-        email: "mgl.deadly@gmail.com",
-        password: (await bcrypt.hash('adopter', 10)),
-        phone: "+5553981189503",
-        role: {
-          create: {
-            name: "PERSON",
-            displayName: "Pessoa"
-          }
-        },
-        address: {
-          create: {
-            street: "Rua Professor Olavo de Carvalho",
-            number: "666",
-            district: "Cohab II",
-            city: "Pelotas",
-            uf: "RS"
-          }
-        }
-      }
-    });
-    
-    await prisma.mutation.createUser({
-      data: {
-        name: "ONG Amigos Dos Animais",
-        email: "amigos.ong@amigos.com",
-        password: (await bcrypt.hash('adopter', 10)),
-        phone: "+5553981189503",
-        cnpj: "011.145.1400-600",
-        role: {
-          create: {
-            name: "INSTITUTION",
-            displayName: "Instituição"
-          }
-        },
-        address: {
-          create: {
-            street: "Rua José de Alencar",
-            number: "55",
-            complement: "A",
-            district: "Azenha",
-            city: "Porto Alegre",
-            uf: "RS"
-          }
-        }
-      }
-    });
+    log(chalk.yellow('Creating User Roles...'));
+    await createRoles();
     console.log("OK");
+    log(chalk.yellow('Creating Users...'));
+    await createUsers();
   } catch(err) {
     throw new Error(err);
   }
