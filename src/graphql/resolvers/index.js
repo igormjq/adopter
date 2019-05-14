1bd32b382f5030ea751cd92254a4b03be041f43c
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-14 10:36:05 -0300

    :bug: Fix AuthPayload resolver to fetch user relations

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -4,4 +4,7 @@ import Mutation from './Mutation';
 export default {
   Query,
   Mutation,
+  AuthPayload: {
+    user: async ({ user: { id }}, args, { prisma }, info) => prisma.query.user({ where: { id }}, info) 
+  }
 }
