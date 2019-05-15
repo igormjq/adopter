367becfaa4fde3bd8bae7df9b6d4f66548ffda0d
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-15 18:31:45 -0300

    :lock: Add 'me' query in order to decode user based on a token

--- src/graphql/resolvers/user/UserQuery.js
+++ src/graphql/resolvers/user/UserQuery.js
@@ -4,5 +4,14 @@ export default {
   },
   async user(parent, { id }, { prisma }, info) {
     return prisma.query.user({ where: { id } }, info);
+  },
+  async me(parent, args, { prisma, request }, info) {
+    const user = await prisma.query.user({
+      where: {
+        id: request.user.id
+      }
+    }, info);
+    
+    return user;
   }
 }
