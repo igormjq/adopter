eda586ae762495eab01abcfbf23b0bd2615d80a2
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-29 16:56:52 -0300

    :new: Apply 'WHERE' filter to animal query

--- src/graphql/resolvers/animal/AnimalQuery.js
+++ src/graphql/resolvers/animal/AnimalQuery.js
@@ -1,6 +1,14 @@
 export default {
-  async animals(parent, { first, skip }, { prisma }, info) {
-    return await prisma.query.animals({ first, skip }, info);
+  async animals(parent, { first, skip, where }, { prisma }, info) {
+    const whereFilter = {
+      ...where,
+      address: {
+        city_contains: where.address.city,
+        uf: where.address.uf
+      }
+    };
+    
+    return await prisma.query.animals({ first, skip, where: whereFilter }, info);
   },
   async animal(parent, { id }, { prisma }, info) {
     return await prisma.query.animal({ where: { id } }, info);
