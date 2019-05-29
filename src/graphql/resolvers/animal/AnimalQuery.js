1293229636e0258f7eaa4ce10339376d44d74717
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-29 19:16:56 -0300

    :new: Add 'where' clause to animal query

--- src/graphql/resolvers/animal/AnimalQuery.js
+++ src/graphql/resolvers/animal/AnimalQuery.js
@@ -1,14 +1,6 @@
 export default {
   async animals(parent, { first, skip, where }, { prisma }, info) {
-    const whereFilter = {
-      ...where,
-      address: {
-        city_contains: where.address.city,
-        uf: where.address.uf
-      }
-    };
-    
-    return await prisma.query.animals({ first, skip, where: whereFilter }, info);
+    return await prisma.query.animals({ first, skip, where }, info);
   },
   async animal(parent, { id }, { prisma }, info) {
     return await prisma.query.animal({ where: { id } }, info);
