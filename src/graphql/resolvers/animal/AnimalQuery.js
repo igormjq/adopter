c1070f89824bba0c74eb5c6573d836bd4a4e3da3
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-29 21:11:54 -0300

    :construction: WIP

--- src/graphql/resolvers/animal/AnimalQuery.js
+++ src/graphql/resolvers/animal/AnimalQuery.js
@@ -1,5 +1,6 @@
 export default {
   async animals(parent, { first, skip, where }, { prisma }, info) {
+    console.log(where);
     return await prisma.query.animals({ first, skip, where }, info);
   },
   async animal(parent, { id }, { prisma }, info) {
