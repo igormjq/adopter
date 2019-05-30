2f9ce8ecdde1385651526d0f7ff0c57d93297bb5
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-29 22:36:29 -0300

    :recycle: Refactor query to support search filters

--- src/graphql/resolvers/animal/AnimalQuery.js
+++ src/graphql/resolvers/animal/AnimalQuery.js
@@ -1,7 +1,6 @@
 export default {
   async animals(parent, { first, skip, where }, { prisma }, info) {
-    console.log(where);
-    return await prisma.query.animals({ first, skip, where }, info);
+    return await prisma.query.animals({ first, skip, where: where ? where : {} }, info);
   },
   async animal(parent, { id }, { prisma }, info) {
     return await prisma.query.animal({ where: { id } }, info);
