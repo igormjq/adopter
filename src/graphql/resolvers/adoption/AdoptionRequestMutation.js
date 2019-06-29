7fbb5fc2f36b8a38b599d2accfcb76e06cd7c218
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-06-29 18:41:59 -0300

    :sparkles: Implements mutation to create adoption request

--- src/graphql/resolvers/adoption/AdoptionRequestMutation.js
+++ src/graphql/resolvers/adoption/AdoptionRequestMutation.js
@@ -1,3 +1,18 @@
 export default {
-  
-}
+  async createAdoptionRequest(parent, { animalId }, { prisma, request }, info) {
+    return await prisma.mutation.createAdoptionRequest({
+      data: {
+        animal: {
+          connect: {
+            id: animalId
+          }
+        },
+        sentBy: {
+          connect: {
+            id: request.user.id
+          }
+        }
+      }
+    }, info);
+  }
+};
