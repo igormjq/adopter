b8d777310ec8eeda246c8f594fc9e27e4479cac1
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-07-05 16:59:01 -0300

    :card_file_box: Seeders

--- db/seeder/create-animals.js
+++ db/seeder/create-animals.js
@@ -41,6 +41,33 @@ export default async () => {
         }
       },
     });
+
+    await prisma.mutation.createAnimal({
+      data: {
+        profileImg: "https://s2.glbimg.com/z38cvd1l6M_nk8lFU2GyCXhdeLE=/0x0:1280x720/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/3/j/7APRrpRSyBNYLBjFFciA/whatsapp-image-2019-02-22-at-14.53.01.jpeg",
+        type: 'DOG',
+        size: 'SMALL',
+        gender: 'MALE',
+        ageGroup: 'ADULT',
+        castrated: getRandom(),
+        vaccinated: getRandom(),
+        dewormed: getRandom(),
+        specialNeeds: false,
+        name: "Cliff",
+        about: "O cachorro caramelo à esquerda chama-se CLiff. Muito brincalhão, leal e seu prato favorito é... resto de arroz carreteiro. Não dispensa uma boa ração. Ele gosta muito das suas irmãs Anabele e Alemoa, as quais devem ser adotadas juntas. Afinal, família unida jamais será vencida!",
+        address: {
+          create: {
+            city: getRandom(addresses),
+            uf: 'RS'
+          }
+        },
+        owner: {
+          connect: {
+            email: "igormjq@gmail.com"
+          }
+        }
+      },
+    });
   }
   
   for(let i=0; i < 30; i++) {
