86b57aeb7d69f8746a4db1617100e206b9e8f18f
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-07-25 18:11:18 -0300

    :recycle: Clears Heroku

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,7 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
   typeDefs: './src/generated/prisma.graphql',
-  endpoint: process.env.PRISMA_ENDPOINT
+  endpoint: 'http://localhost:4466'
 });
 
 export default prisma;
--- src/www/index.js
+++ src/www/index.js
@@ -1,5 +1,3 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
 import 'universal-dotenv'
 import app from '../app';
 
